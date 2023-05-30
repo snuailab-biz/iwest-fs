e278ad1631a3067d2ea17330f8557243353a763b
JongJin Lee <116264357+leejongjin2@users.noreply.github.com>
2023-05-30 16:07:53 +0900

    back

--- setup.py
+++ setup.py
@@ -1,68 +1,15 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-
-
 import pkg_resources as pkg
 FILE = Path(__file__).resolve()
 PARENT = FILE.parent  # root directory
 README = (PARENT / 'README.md').read_text(encoding='utf-8')
 REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
-import os
-import glob
-
-
-def get_extensions():
-    import torch
-
-    from torch.utils.cpp_extension import CUDA_HOME
-    from torch.utils.cpp_extension import CppExtension
-    from torch.utils.cpp_extension import CUDAExtension
-
-    requirements = ["torch", "torchvision"]
-    this_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'iwestfs/models/ops')
-    extensions_dir = os.path.join(this_dir, "src")
-
-    main_file = glob.glob(os.path.join(extensions_dir, "*.cpp"))
-    source_cpu = glob.glob(os.path.join(extensions_dir, "cpu", "*.cpp"))
-    source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
-
-    sources = main_file + source_cpu
-    extension = CppExtension
-    extra_compile_args = {"cxx": []}
-    define_macros = []
-
-    if torch.cuda.is_available() and CUDA_HOME is not None:
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-    else:
-        raise NotImplementedError('Cuda is not availabel')
-
-    sources = [os.path.join(extensions_dir, s) for s in sources]
-    include_dirs = [extensions_dir]
-    ext_modules = [
-        extension(
-            "MultiScaleDeformableAttention",
-            sources,
-            include_dirs=include_dirs,
-            define_macros=define_macros,
-            extra_compile_args=extra_compile_args,
-        )
-    ]
-    return ext_modules
-
-
 setup(
     name = 'iwestfs',
-    version = '0.0.5',
+    version = '0.0.1',
     description='setup iwest fire & smoke detector install.',
     url='https://github.com/snuailab-biz/iwest-fs.git',
     author='leejj',
@@ -70,7 +17,5 @@ setup(
     license='snuailab',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=REQUIREMENTS,
-    ext_modules=get_extensions(),
-    cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
+    install_requires=REQUIREMENTS
 )
