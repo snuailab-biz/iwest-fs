4306648e48cfa9938bfe38e69de0b295907d78de
JongJin Lee <116264357+leejongjin2@users.noreply.github.com>
2023-05-30 15:23:54 +0900

    setup

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ README = (PARENT / 'README.md').read_text(encoding='utf-8')
 REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
 setup(
-    name = 'rfai',
-    version = '0.0.4',
-    description='setup rfai install.',
-    url='https://github.com/RearFold/RearFold-AI.git',
-    author='kaejong2',
-    author_email="kaejong2@gmail.com",
-    license='ljj',
+    name = 'iwestfs',
+    version = '0.0.1',
+    description='setup iwest fire & smoke detector install.',
+    url='https://github.com/snuailab-biz/iwest-fs.git',
+    author='leejj',
+    author_email="leejj@snuailab.ai",
+    license='snuailab',
     packages=find_packages(),
     include_package_data=True,
     install_requires=REQUIREMENTS
