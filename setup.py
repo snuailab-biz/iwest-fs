9c964f14d85d43c03f8bebfa6f2c4c78f67ed303
JongJin Lee <116264357+leejongjin2@users.noreply.github.com>
2023-05-30 15:54:08 +0900

    set

--- setup.py
+++ setup.py
@@ -1,15 +1,27 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-
+from setuptools.command.install import install
+import subprocess
 import pkg_resources as pkg
+
 FILE = Path(__file__).resolve()
 PARENT = FILE.parent  # root directory
 README = (PARENT / 'README.md').read_text(encoding='utf-8')
 REQUIREMENTS = [f'{x.name}{x.specifier}' for x in pkg.parse_requirements((PARENT / 'requirements.txt').read_text())]
 
+class CustomInstall(install):
+    def run(self):
+        # 기본 설치 작업 실행
+        install.run(self)
+        
+        # make.sh 스크립트 실행
+        subprocess.call(['pwd'])
+        # subprocess.call(['sh', 'make.sh'])
+        # subprocess.call(['sh', 'make.sh'])
+
 setup(
     name = 'iwestfs',
-    version = '0.0.1',
+    version = '0.0.2',
     description='setup iwest fire & smoke detector install.',
     url='https://github.com/snuailab-biz/iwest-fs.git',
     author='leejj',
@@ -17,5 +29,8 @@ setup(
     license='snuailab',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=REQUIREMENTS
+    install_requires=REQUIREMENTS,
+    cmdclass={
+        'install': CustomInstall,
+    },
 )
